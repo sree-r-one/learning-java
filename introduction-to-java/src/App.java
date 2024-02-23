dbb7cfa911c37c3024b39753e9f0a661169f0ee4
sree-r-one <sreeraj_ec@hotmail.com>
2024-02-23 15:50:52 +0800

    classes added

--- introduction-to-java/src/App.java
+++ introduction-to-java/src/App.java
@@ -1,21 +1,13 @@
-import java.util.Scanner;
-
-import chapter1.Assignment1;
+import chapter2.Bank;
 
 public class App {
 
-    public static Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) throws Exception {
-        String fullName = Assignment1.sayHello();
-        Assignment1.addFiveNumbers(scanner);
-        Assignment1.isOddEven(scanner);
-        Assignment1.primeNumberChecker(scanner);
-        Assignment1.secondsToHoursConverter(scanner);
-        Assignment1.sayGoodbye(fullName);
-
-        scanner.close();
-    }
-
     
+        // Create new instance of the bank class 
+        Bank bank = new Bank();
+        
+        // Call the run method from the bank class
+        bank.run();
+    }
 }
