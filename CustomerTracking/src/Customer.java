44043a10360135a7585d316d56ea06a410d7f002
sree-r-one <sreeraj_ec@hotmail.com>
2024-03-07 10:30:39 +0800

    customer class added

--- CustomerTracking/src/Customer.java
+++ CustomerTracking/src/Customer.java
@@ -1,7 +1,41 @@
+import java.util.ArrayList;
+
 /**
- * Customer
+ * Represent customer with name, ID and geography
  */
 public class Customer {
 
+    // Static variables 
+
+    /** 
+     * Company for all customers
+    */
+    static final String COMPANY = "CVS";
+
+    /** 
+     * Store list of all customers 
+    */
+    static ArrayList<Customer> CUSTOMERS = new ArrayList<Customer>();
+
+    /**
+     * Generate and keep track of custoemr IDs
+     */
+    static Counter COUNTER; 
+
+    // Instance variables 
+
+    /**
+     * Name of the customer
+     */
+    String name; 
+
+    /**
+     * Geography of the customer 
+     */
+    String geography;
+
+    /**
+     * ID of the customer
+     */
     
 }
