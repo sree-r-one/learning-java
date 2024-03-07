30f90efe2c820676c73bf983952109769d899e48
sree-r-one <sreeraj_ec@hotmail.com>
2024-03-08 02:21:20 +0800

    vehicle class

---
+++ Vehicle/src/Vehicle.java
@@ -0,0 +1,25 @@
+/**
+ * Defines a vehicle class
+ */
+public class Vehicle {
+    // Variables 
+
+    /**
+     * Number of passengers
+     */
+    int passengers;   
+
+    /**
+     * Fuel capacity in gallons 
+     */
+    int fuelcap; 
+
+    /**
+     * Fuel consumption in miles per gallon 
+     */
+    int mpg; 
+
+    // Constructor 
+
+    // Methods 
+}
