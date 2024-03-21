e3585734aa69e5c435652ac4acc4be9cff9d29ec
sree-r-one <sreeraj_ec@hotmail.com>
2024-03-22 01:17:39 +0800

    Inheritance in week 1

--- Animal/src/Animal.java
+++ Animal/src/Animal.java
@@ -1,11 +1,75 @@
 public class Animal {
 
-    public void greeting(){
-        System.out.println("I am an animal");
+    // static variables
+
+    /**
+     * Default age of the animal
+     */
+    static int DEFAULT_AGE = 0;
+
+    // instance variables 
+
+    /**
+     * Age of the animal
+     */
+    int age; 
+
+    /**
+     * Weight of the animal
+     */
+    double weight;
+
+    /**
+     * Name of the animal 
+     */
+    String name; 
+
+    // constructors
+
+    /**
+     * Creates an animal with a given age
+     * @param age of the animal
+     */
+    public Animal(int age){
+        this.age = age;
     }
 
-    public static void main(String[] args){
-        Dog dog = new Dog();
-        dog.greeting();
+    public Animal(){
+        // Calls first constructor with the default value 
+        this(Animal.DEFAULT_AGE);
+    }
+
+    // getters and setters
+
+    /**
+     * Gets the name of the animal
+     * @return name of the animal
+     */
+    public String getName(){
+        return this.name;
+    }
+
+    /**
+     * Sets name of the animal
+     * @param name
+     */
+    public void setName(String name){
+        this.name = name; 
+    }
+
+    /**
+     * Animal speaks
+     */
+    public void speak(){
+        System.out.println("Animal says hi");
+    }
+
+    /**
+     * Overrides the toString Object class 
+     * Returns the name of the animal class for printing 
+     */
+    @Override
+    public String toString(){
+        return this.name;
     }
 }
