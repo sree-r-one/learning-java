6f32c4ae1e31805967b45f73f6b420adc34c99fa
sree-r-one <sreeraj_ec@hotmail.com>
2024-03-21 22:32:17 +0800

    Inheritance

---
+++ Animal/src/Animal.java
@@ -0,0 +1,11 @@
+public class Animal {
+
+    public void greeting(){
+        System.out.println("I am an animal");
+    }
+
+    public static void main(String[] args){
+        Dog dog = new Dog();
+        dog.greeting();
+    }
+}
---
+++ Animal/src/Dog.java
@@ -0,0 +1,10 @@
+public class Dog extends Animal {
+
+    @Override
+    public void greeting(){
+
+        super.greeting();
+        System.out.println("I am a dog");
+    }
+    
+}
