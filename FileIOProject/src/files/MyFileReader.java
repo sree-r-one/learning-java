22e03f3bda72266101dba364386443fef2f267d8
sree-r-one <sreeraj_ec@hotmail.com>
2024-03-30 23:29:08 +0800

    exception handling

---
+++ FileIOProject/src/files/MyFileReader.java
@@ -0,0 +1,27 @@
+package files;
+
+import java.io.FileNotFoundException;
+
+/**
+ * Utility class for reading particular types of files
+ */
+public class MyFileReader {
+
+    /**
+     * Calculates the total sum of numbers in the given file
+     * Returns 0.0 if there are no numerical values found, or there is an error or
+     * exception
+     * 
+     * @param fileName to read
+     * @return sum of all numbers in the file
+     * @throws FileNotFoundException if file not found
+     */
+    public static double readFileGetTotalSum(String fileName) throws FileNotFoundException {
+
+        // Create file
+
+        // Create default sum value
+
+        return 0.0;
+    }
+}
