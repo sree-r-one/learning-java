50ba45f00bc7eb29d9b1c22db916d8887c4947f8
sree-r-one <sreeraj_ec@hotmail.com>
2024-04-03 23:57:01 +0800

    collections learning

--- Collections/src/CollectionsClass.java
+++ Collections/src/CollectionsClass.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 
 /**
  * Class with various methods for using different kinds of collections
@@ -16,6 +18,31 @@ public class CollectionsClass {
      * @param max
      */
     public static void removeRange(ArrayList<Integer> list, int min, int max) {
-        //
+
+        // Create iterator to interate over list and remove items in place
+        Iterator<Integer> iterator = list.iterator();
+
+        while (iterator.hasNext()) {
+            Integer next = iterator.next();
+            if (next >= min && next <= max) {
+                iterator.remove();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // Create array of ints
+        Integer[] removeRangeArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+
+        // Add all items to arraylist
+        arrayList.addAll(Arrays.asList(removeRangeArray));
+
+        CollectionsClass.removeRange(arrayList, 3, 6);
+
+        System.out.println("removeRange : " + arrayList);
+        System.out.println();
+
     }
 }
