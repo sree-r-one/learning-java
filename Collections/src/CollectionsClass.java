d964a4c3447239d3bcd35df8232f8914dd47fb12
sree-r-one <sreeraj_ec@hotmail.com>
2024-04-03 23:31:38 +0800

    commit of the day

---
+++ Collections/src/CollectionsClass.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+/**
+ * Class with various methods for using different kinds of collections
+ */
+public class CollectionsClass {
+
+    /**
+     * Takes a give nlist and removes elements in the range min through max
+     * (inclusive)
+     * 
+     * Demonstrates use of iterator
+     * 
+     * @param list of values
+     * @param min
+     * @param max
+     */
+    public static void removeRange(ArrayList<Integer> list, int min, int max) {
+        //
+    }
+}
