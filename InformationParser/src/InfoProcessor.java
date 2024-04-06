7d1c7c8c58436aaa726f23faa798374b707378f7
sree-r-one <sreeraj_ec@hotmail.com>
2024-04-06 13:09:28 +0800

    commit of the day

--- InformationParser/src/InfoProcessor.java
+++ InformationParser/src/InfoProcessor.java
@@ -13,8 +13,61 @@ public class InfoProcessor {
     ArrayList<String> lines = new ArrayList<String>();
 
     // Constructor
-    public InfoProcessor(ArrayList<String> lines) {
 
+    /**
+     * Creates InfoProcessor with given list of lines
+     * 
+     * @param lines to process
+     */
+    public InfoProcessor(ArrayList<String> lines) {
+        this.lines = lines;
+    }
+
+    /**
+     * Gets course Name from a list of lines
+     * 
+     * @return couseName
+     */
+    public String getCourseName() {
+        return getNextStringStartsWith("Course:");
+    }
+
+    /**
+     * Gets the course ID from a list of lines
+     * 
+     * @return courseID
+     */
+    public int getCourseID() {
+        String strCourseID = getNextStringStartsWith("CourseID");
+        int courseID = Integer.parseInt(strCourseID);
+        return courseID;
+    }
+
+    /**
+     * Gets Student ID from a list of lines
+     * 
+     * @return
+     */
+    public int getStudentId() {
+        String strStudentID = getNextStringStartsWith("StudentID");
+        int studentID = Integer.parseInt(strStudentID);
+        return studentID;
+    }
+
+    /**
+     * 
+     * @param str to look for in lines
+     * @return String from the very next line
+     */
+    String getNextStringStartsWith(String string) {
+
+        for (int i = 0; i < lines.size() - 1; i++) {
+            if (lines.get(i).startsWith(string)) {
+                return lines.get(i + 1);
+            }
+        }
+
+        return null;
     }
 
 }
--- InformationParser/src/Main.java
+++ InformationParser/src/Main.java
@@ -3,11 +3,29 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
+        /**
+         * Creates a new instance of FileReader which reads the file "info.txt"
+         */
         MyFileReader fr = new MyFileReader("info.txt");
 
+        /**
+         * Clean lines of text passed from the file reader
+         */
         ArrayList<String> lines = fr.getCleanContent();
 
+        /**
+         * Create new instance of info processor
+         */
+        InfoProcessor ip = new InfoProcessor(lines);
+
+        /**
+         * Get course name
+         */
+        String courseName = ip.getCourseName();
+
+        // >>>>>>
         System.out.println(lines);
+        System.out.println(courseName);
 
     }
 }
