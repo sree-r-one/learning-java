9250f9a324ed00763e39b345f7b1db28f0008ce3
sree-r-one <sreeraj_ec@hotmail.com>
2024-04-09 11:09:58 +0800

    regex practice

--- RegexProject/src/RegexClass.java
+++ RegexProject/src/RegexClass.java
@@ -1,8 +1,36 @@
-
+/**
+ * Contains various methods for parsing Strings based on regular expressions
+ */
 public class RegexClass {
 
+	/**
+	 * Prints a given array of Strings
+	 * 
+	 * @param arr to print
+	 */
+	public static void printTokens(String[] arr) {
+		System.out.println("Printing Tokens");
+		for (String str : arr) {
+			System.out.print(str + ", ");
+		}
+		System.out.println();
+	}
+
+	/**
+	 * Splits the given string based on the given regex pattern
+	 * 
+	 * @param str   to split
+	 * @param regex pattern to match
+	 * @return String array of tokens
+	 */
+	public static String[] splitString(String str, String regex) {
+		return str.split(regex);
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String str = "The cow jumped over the moon";
+		String[] tokens = RegexClass.splitString(str, " ");
+		RegexClass.printTokens(tokens);
 
 	}
 
